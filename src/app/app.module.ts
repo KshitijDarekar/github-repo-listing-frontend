c69ac4f9621401d950590f1f847c640375b9c3b3
Kshitij Darekar <kshitijdarekar13@gmail.com>
2023-01-16 00:32:52 +0530

    add and update dependencies;configure packages , dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
+import { GithubProfileDetailsComponent } from './components/github-profile-details/github-profile-details.component';
+import { UserRepositoryComponent } from './components/user-repository/user-repository.component';
+import { SkeletonLoaderComponent } from './components/skeleton-loader/skeleton-loader.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { NgxPaginationModule } from 'ngx-pagination';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubProfileDetailsComponent,
+    UserRepositoryComponent,
+    SkeletonLoaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSkeletonLoaderModule,
+    NgxPaginationModule,
+    NgbModule ,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
